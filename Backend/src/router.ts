0964aa419122e8bf2cb661c79ea28edcbe1d22a2
Antônio Carvalho <carneiroantonio3008@gmail.com>
2025-11-06 21:25:43 -0300

    atualizados

--- Backend/src/router.ts
+++ Backend/src/router.ts
@@ -6,6 +6,5 @@ export const router = express.Router();
 router.get('/', usuarioController.getAll);
 router.get('/usuario/:id', usuarioController.getById);
 router.post('/usuario', usuarioController.newUsuario);
-router.put('/usuario/:id', usuarioController.editUsuario);
 router.patch('/usuario/:id', usuarioController.editPartial);
 router.delete('/usuario/:id', usuarioController.removeUsuario);
