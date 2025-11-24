5fb8a85f99acc4026896ad79a8b1fae8b16ee47a
Antônio Carvalho <carneiroantonio3008@gmail.com>
2025-11-24 17:24:18 -0300

    Atualizei o Tailwind

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./public/**/*.html",
+    "./public/**/*.js",
+    "./src/**/*.js",
+    "./src/**/*.html"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
